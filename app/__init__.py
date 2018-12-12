8e6a91eb8136a14e757e55fc6ee6d64deb776d7b
NgeKaworu <NgeKaworu@163.com>
2018-12-12 16:04:19 +0800

    不用全局g

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,12 @@
-import os
-
 from flask import Flask, Blueprint
 
-
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-    
+    #注册页面
     from app import author
     app.register_blueprint(author.bp)
+
 
     return app
