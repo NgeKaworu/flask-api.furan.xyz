dde342e5b7b89f0d0cd994ec2d0ef4a95816701a
NgeKaworu <NgeKaworu@163.com>
2018-12-12 16:02:24 +0800

    Revert "增加关闭数据库"
    This reverts commit cfd3fa3b9e9c167447bcf4fbf18fcb9bebc3d4a1.

--- app/db.py
+++ app/db.py
@@ -1,36 +1,19 @@
 import redis
 import pymongo
-from flask import g
 
 _password = 'FRO0o0<,>.'
 
 
 def getRedis():
-    if 'redisDB' not in g:
-        pool = redis.ConnectionPool(
-            host='localhost', port=6379, decode_responses=True, password=_password)
+    pool = redis.ConnectionPool(
+        host='localhost', port=6379, decode_responses=True, password=_password)
 
-        g.redisDB = redis.Redis(connection_pool=pool)
+    r = redis.Redis(connection_pool=pool)
 
-    return g.redisDB
+    return r
 
 
 def getMongo():
-    if 'mongoDB' not in g:
-            g.mongoDB = pymongo.MongoClient(
-                'localhost:27017', username='furan', password=_password)
-    return g.mongoDB
-
-
-def close_db(e=None):
-    redisDB = g.pop('redisDB', None)
-    if redisDB:
-        redisDB.connection_pool.disconnect()
-    
-    mongoDB = g.pop('mongoDB', None)
-    if mongoDB:
-        mongoDB.close()
-
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
+    mongoDB = pymongo.MongoClient(
+          'localhost:27017', username='furan', password=_password)
+    return mongoDB
