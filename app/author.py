5a7ccf1badc39927c665fe403f9799058bb12ae3
NgeKaworu <NgeKaworu@163.com>
2018-12-18 00:25:52 +0800

    描述完成

--- app/author.py
+++ app/author.py
@@ -6,25 +6,47 @@ from bson.json_util import dumps
 bp = Blueprint('author', __name__, url_prefix='/author')
 
 _mongodb = 'quotes'
-_collections = 'author'
 
 redisDB = getRedis()
 mongoClient = getMongo()
 
 mongoDB = mongoClient[_mongodb]
-mongoCol = mongoDB[_collections]
 
 
 @bp.route('/getCount', methods=['GET'])
 def getCount():
-    redisCance = redisDB.get(_collections)
-    if redisCance:
-        return jsonify(redisCance)
+    _collections = 'author'
+    mongoCol = mongoDB[_collections]
+    redisCache = redisDB.get(_collections)
+    if redisCache:
+        return jsonify(redisCache)
     else:
         mongoData = [i for i in mongoCol.find()]
         if mongoData:
             jsonData = dumps(mongoData)
             redisDB.set(_collections, jsonData)
+            return jsonify(jsonData)
+        else:
+          return jsonify({'resulte': 'No Data'})
+
+
+@bp.route('/getAbout', methods=['GET'])
+def getAbout():
+    _collections = 'about'
+    mongoCol = mongoDB[_collections]
+
+    param = request.args.get('author')
+
+    cacheName = 'About' + param
+    redisCache = redisDB.get(cacheName)
+
+    if redisCache:
+        return jsonify(redisCache)
+    else:
+        mongoData = mongoCol.find_one({ 'author': param })
+        if mongoData:
+            jsonData = dumps(mongoData)
+            redisDB.set(cacheName, jsonData)
             return jsonify(jsonData)
         else:
           return jsonify({'resulte': 'No Data'})
