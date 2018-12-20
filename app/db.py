0458766ef8388bd3418b8218f23c2ccca5318733
NgeKaworu <NgeKaworu@163.com>
2018-12-20 22:53:57 +0800

    增加flask配置信息

--- app/db.py
+++ app/db.py
@@ -1,12 +1,10 @@
 import redis
 import pymongo
-
-_password = 'FRO0o0<,>.'
-
+from app.settings import password
 
 def getRedis():
     pool = redis.ConnectionPool(
-        host='localhost', port=6379, decode_responses=True, password=_password)
+        host='localhost', port=6379, decode_responses=True, password=password)
 
     r = redis.Redis(connection_pool=pool)
     return r
@@ -14,5 +12,5 @@ def getRedis():
 
 def getMongo():
     mongoDB = pymongo.MongoClient(
-          'localhost:27017', username='furan', password=_password)
+          'localhost:27017', username='furan', password=password)
     return mongoDB
