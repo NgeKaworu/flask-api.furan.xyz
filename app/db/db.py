34a32cdba29ab08f95f55ee5f74dcb475b1072dd
NgeKaworu <NgeKaworu@163.com>
2019-01-14 09:52:45 +0800

    优化测试

--- app/db/db.py
+++ app/db/db.py
@@ -1,37 +1,22 @@
 import redis
 import pymongo
-import click
-from flask import current_app,  g
+from flask import current_app
 
 
 def getRedis():
-    if 'redis' not in g:
-        pool = redis.ConnectionPool(
-            host='localhost', port=6379, decode_responses=True, password=current_app.config['PASSWORD'])
-        g.redis = redis.Redis(connection_pool=pool)
-        return g.redis
+    pool = redis.ConnectionPool(
+        host='localhost', port=6379, decode_responses=True, password=current_app.config['PASSWORD'])
+    redisdb = redis.Redis(connection_pool=pool)
+    return redisdb
 
 
 def getMongo():
-    if 'mongo' not in g:
-        g.mongo = pymongo.MongoClient(
-            'localhost:27017', username='furan', password=current_app.config['PASSWORD'], connect=False)
-        return g.mongo
-
-
-def close_db(e=None):
-    redis = g.pop('redis', None)
-    mongo = g.pop('mongo', None)
-
-    if redis is not None:
-        redis.connection_pool.disconnect()
-
-    if mongo is not None:
-        mongo.close()
+    mongodb = pymongo.MongoClient(
+        'localhost:27017', username='furan', password=current_app.config['PASSWORD'], connect=False)
+    return mongodb
 
 
 def init_app(app):
     with app.app_context():
         getRedis()
         getMongo()
-    app.teardown_appcontext(close_db)
