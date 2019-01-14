a945dde062bc0af430a83f723ab85840e673218d
NgeKaworu <NgeKaworu@163.com>
2019-01-15 06:41:13 +0800

    重构author

--- app/author/author.py
+++ app/author/author.py
@@ -1,18 +1,28 @@
 from flask import request, Blueprint, jsonify
+from flask_restful import Resource, Api
 from ..db.dao import DAO
 
 bp = Blueprint('author', __name__, url_prefix='/author')
+api_author = Api(bp)
 
 
-@bp.route('/getCount', methods=['GET'])
-def getCount():
-    author = DAO('quotes', 'author')
-    return jsonify(author.get())
+class AuthorCount(Resource):
+    def __init__(self):
+        self.db = DAO('quotes', 'author')
+
+    def get(self):
+        return jsonify(self.db.get())
 
 
-@bp.route('/getAbout', methods=['GET'])
-def getAbout():
-    about = DAO('quotes', 'about')
-    param = request.args.get('author')
-    query = {'author': param}
-    return jsonify(about.get_one(query))
+class AuthorAbout(Resource):
+    def __init__(self):
+        self.db = DAO('quotes', 'about')
+
+    def get(self):
+        param = request.args.get('author')
+        query = {'author': param}
+        return jsonify(self.db.get_one(query))
+
+
+api_author.add_resource(AuthorCount, '/getCount', endpoint='authorcount')
+api_author.add_resource(AuthorAbout, '/getAbout', endpoint='authorabout')
