f606e7d30081d2465f4e08bc893a93779d3f26fe
NgeKaworu <NgeKaworu@163.com>
2019-01-17 12:43:30 +0800

    dao解耦

--- app/db/dao.py
+++ app/db/dao.py
@@ -4,42 +4,74 @@ import json
 from flask import abort
 
 
-class DAO(object):
+class MongoDAO():
     def __init__(self, db, collection):
-        self.redisDB = getRedis()
-        self.redisCol = collection
         self.mongoClient = getMongo()
         self.mongoDB = self.mongoClient[db]
         self.mongoCol = self.mongoDB[collection]
 
     def __del__(self):
         self.mongoClient.close()
+
+    def get(self):
+        mongoData = [i for i in self.mongoCol.find()]
+        if mongoData:
+            # 解析成string bson => string
+            return dumps(mongoData)
+
+    def get_one(self, query):
+        mongoData = self.mongoCol.find_one(query)
+        if mongoData:
+            return dumps(mongoData)
+
+
+class RedisDAO():
+    def __init__(self, collection):
+        self.redisDB = getRedis()
+        self.redisCol = collection
+
+    def __del__(self):
         self.redisDB.connection_pool.disconnect()
 
     def get(self):
         redisCache = self.redisDB.get(self.redisCol)
         if redisCache:
+            return redisCache
+
+    def get_one(self, query):
+        redisCache = self.redisDB.get(query)
+        if redisCache:
+            return redisCache
+
+
+class DAO(MongoDAO, RedisDAO):
+    def __init__(self, db, collection):
+        MongoDAO.__init__(self, db, collection)
+        RedisDAO.__init__(self, collection)
+
+    # 先读redis, redis没有就读mongo并且写入redis, 都没有就返回404
+    def get(self):
+        redisCache = RedisDAO.get(self)
+        if redisCache:
             return json.loads(redisCache)
         else:
-            mongoData = [i for i in self.mongoCol.find()]
+            mongoData = MongoDAO.get(self)
             if mongoData:
-                jsonData = dumps(mongoData)
-                self.redisDB.set(self.redisCol, jsonData)
-                return json.loads(jsonData)
+                self.redisDB.set(self.redisCol, mongoData)
+                return json.loads(mongoData)
             else:
                 abort(404)
 
     def get_one(self, query):
         queryVal, = query.values()
         withQuery = self.redisCol + queryVal
-        redisCache = self.redisDB.get(withQuery)
+        redisCache = RedisDAO.get_one(self, withQuery)
         if redisCache:
             return json.loads(redisCache)
         else:
-            mongoData = self.mongoCol.find_one(query)
+            mongoData = MongoDAO.get_one(self, query)
             if mongoData:
-                jsonData = dumps(mongoData)
-                self.redisDB.set(withQuery, jsonData)
-                return json.loads(jsonData)
+                self.redisDB.set(withQuery, mongoData)
+                return json.loads(mongoData)
             else:
                 abort(404)
