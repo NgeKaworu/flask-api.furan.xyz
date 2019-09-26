926e0656ddca406853a064cd277ff7957bd3d5df
宁泰霖 <NgeKaworu@163.com>
2019-09-26 15:59:59 +0800

    tls证书

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from app import create_app
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'], threaded=True)
+    app.run(debug=app.config['DEBUG'], threaded=True, ssl_context=('/etc/letsencrypt/live/furan.xyz/fullchain.pem',
+'/etc/letsencrypt/live/furan.xyz/privkey.pem'))
